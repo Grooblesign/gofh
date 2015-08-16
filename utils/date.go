f3042e57f1cac2260add73551e8c9efb5df1cb1b
Grooblesign <p.j.garner@gmail.com>
2015-08-16 20:29:42 +0100

    refactoring

--- utils/date.go
+++ utils/date.go
@@ -12,15 +12,11 @@ import (
 
 func GetDateValue(date string) int64 {
 
-	// fmt.Println("-------------")
-	// fmt.Println(date)
-
 	var adjustment int64
+	var dateValue int64 = 99999999
 	var err error
 	var match bool
-	var value int64 = 99999999
-	var value1 int64
-	var value2 int64
+	var value int64
 
 	months := "JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC"
 
@@ -43,43 +39,39 @@ func GetDateValue(date string) int64 {
 
 	match, _ = regexp.MatchString("^[0-9]{4}", date)
 	if match {
-		value1, err = strconv.ParseInt(date[0:4], 10, 64)
-		value = value1 * 10000
+		value, err = strconv.ParseInt(date[0:4], 10, 64)
+		dateValue = value * 10000
 		CheckErr(err)
 	}
 
 	match, _ = regexp.MatchString("^Q[0-9]{1} ([0-9]{4})$", date)
 	if match {
-		value1, err = strconv.ParseInt(date[3:7], 10, 64)
+		value, err = strconv.ParseInt(date[3:7], 10, 64)
 		CheckErr(err)
-		value2, err = strconv.ParseInt(date[1:2], 10, 64)
-		CheckErr(err)
+		dateValue = (value * 10000) + 100
 
-		value = (value1 * 10000) + 100 + ((value2 - 1) * 300)
+		value, err = strconv.ParseInt(date[1:2], 10, 64)
+		CheckErr(err)
+		dateValue = dateValue + ((value - 1) * 300)
 	}
 
 	match, _ = regexp.MatchString("^[0-9]{2} [a-zA-Z]{3} ([0-9]{4})$", date)
 	if match {
-		value1, err = strconv.ParseInt(date[7:11], 10, 64)
-		value = (value1 * 10000)
-		value2, err = strconv.ParseInt(date[0:2], 10, 64)
-		value = (value + value2)
-		value = value + (int64)(((strings.Index(months, strings.ToUpper(date[3:6]))/3)+1)*100)
+		value, err = strconv.ParseInt(date[7:11], 10, 64)
+		dateValue = (value * 10000)
+		value, err = strconv.ParseInt(date[0:2], 10, 64)
+		dateValue = (dateValue + value)
+		dateValue = dateValue + (int64)(((strings.Index(months, strings.ToUpper(date[3:6]))/3)+1)*100)
 	}
 
 	match, _ = regexp.MatchString("^[a-zA-Z]{3} ([0-9]{4})", date)
 	if match {
-		value1, err = strconv.ParseInt(date[4:8], 10, 64)
-		value = (value1 * 10000)
-		value = value + (int64)(((strings.Index(months, strings.ToUpper(date[0:3]))/3)+1)*100)
+		value, err = strconv.ParseInt(date[4:8], 10, 64)
+		dateValue = (value * 10000)
+		dateValue = dateValue + (int64)(((strings.Index(months, strings.ToUpper(date[0:3]))/3)+1)*100)
 	}
 
-	value = value + adjustment
+	dateValue = dateValue + adjustment
 
-	// fmt.Println(value)
-	// fmt.Println("-------------")
-
-	return value
+	return dateValue
 }
-
-
